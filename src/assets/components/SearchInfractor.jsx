0201d0172b9058ec6bae922be9d61cbed3820038
Nahuewe <nahuelsoriap@gmail.com>
2025-10-09 12:24:47 -0300

    .

--- src/assets/components/SearchInfractor.jsx
+++ src/assets/components/SearchInfractor.jsx
@@ -23,6 +23,39 @@ function SearchInfractor ({ resetFiltro, onSelectPersona }) {
   })
 
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const cuit = params.get('cuit')
+    const patente = params.get('patente')
+
+    const valor = cuit || patente
+    if (valor && valor.length > 2) {
+      setSearchInput(valor)
+      setHasSelectedPersona(false)
+      setShow(true)
+
+      const valorCorto = valor.slice(0, -1)
+      setSearch(valorCorto)
+
+      setTimeout(() => {
+        setSearch(valor)
+        setIsTyping(false)
+      }, 0)
+    }
+  }, [])
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const cuit = params.get('cuit')
+    const patente = params.get('patente')
+    const valorInicial = cuit || patente
+
+    if (valorInicial && personas.length === 1 && !hasSelectedPersona && !isFetching) {
+      const personaEncontrada = personas[0]
+      selectPersona(personaEncontrada)
+    }
+  }, [personas, hasSelectedPersona, isFetching])
+
+  useEffect(() => {
     if (search.length > 2 && !hasSelectedPersona) {
       const timeoutId = setTimeout(() => {
         refetch()
@@ -78,72 +111,67 @@ function SearchInfractor ({ resetFiltro, onSelectPersona }) {
   }
 
   return (
-    <>
-      <div className='mb-4 relative'>
-        <TextInput
-          name='search'
-          type='text'
-          maxLength={11}
-          placeholder='CUIL'
-          value={searchInput}
-          onChange={handleSearchChange}
-        />
+    <div className='mb-4 relative'>
+      <TextInput
+        name='search'
+        type='text'
+        maxLength={11}
+        placeholder='CUIL'
+        value={searchInput}
+        onChange={handleSearchChange}
+      />
 
-        <div type='button' className='absolute top-3 right-3'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='icon icon-tabler icon-tabler-search dark:stroke-white'
-            width='16'
-            height='16'
-            viewBox='0 0 24 24'
-            strokeWidth='1.5'
-            stroke='#000000'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0' />
-            <path d='M21 21l-6 -6' />
-          </svg>
-        </div>
-
-        {search.length >= 2 && show && (
-          <ul className='w-full max-h-52 overflow-y-auto absolute z-10 bg-white shadow-md'>
-            {isTyping || isFetching
-              ? (
-                <li className='hover:bg-slate-300 border-b-2 border-x px-2 py-2'>
-                  Buscando personas...
-                </li>
-                )
-              : personas.length > 0
-                ? (
-                    personas.map((per) => (
-                      <li
-                        key={per.id}
-                        className='hover:bg-slate-300 border-b-2 border-x px-2'
-                      >
-                        <button
-                          type='button'
-                          className='mb-1 w-full text-start py-2'
-                          onClick={() => selectPersona(per)}
-                        >
-                          <strong>{per.cuit || per.dni}</strong> -{' '}
-                          {deleteDuplicateName(per.apellido, per.nombre)}
-                        </button>
-                      </li>
-                    ))
-                  )
-                : (
-                  <li className='hover:bg-slate-300 border-b-2 border-x px-2'>
-                    <strong>No se encontró a la persona.</strong><br />
-                    <p className='text-sm text-gray-500'>Por favor, acércate al Juzgado de Faltas Municipal, Ubicado en la calle Maipu Norte 550 de 07:00 AM hasta 16:00 PM ante cualquier duda.</p>
-                  </li>
-                  )}
-          </ul>
-        )}
+      <div type='button' className='absolute top-3 right-3'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='icon icon-tabler icon-tabler-search dark:stroke-white'
+          width='16'
+          height='16'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='#000000'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+          <path d='M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0' />
+          <path d='M21 21l-6 -6' />
+        </svg>
       </div>
-    </>
+
+      {search.length >= 2 && show && (
+        <ul className='w-full max-h-52 overflow-y-auto absolute z-10 bg-white shadow-md'>
+          {isTyping || isFetching
+            ? (
+              <li className='hover:bg-slate-300 border-b-2 border-x px-2 py-2'>
+                Buscando personas...
+              </li>
+              )
+            : personas.length > 0
+              ? (
+                  personas.map((per) => (
+                    <li
+                      key={per.id}
+                      className='hover:bg-slate-300 border-b-2 border-x px-2'
+                    >
+                      <button
+                        type='button'
+                        className='mb-1 w-full text-start py-2'
+                        onClick={() => selectPersona(per)}
+                      >
+                        <strong>{per.cuit || per.dni}</strong> -{' '}
+                        {deleteDuplicateName(per.apellido, per.nombre)}
+                      </button>
+                    </li>
+                  ))
+                )
+              : (
+                <div />
+                )}
+        </ul>
+      )}
+    </div>
   )
 }
 
